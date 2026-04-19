cfbe030867c8368104304203e284a43dcbc1711a
MohaMed NabiL <49358099+mohamedN2018@users.noreply.github.com>
2026-04-19 11:22:37 +0200

    add a new function

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    's_users',
+    'rest_framework',
+    'corsheaders',
 ]
 
+CROS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
